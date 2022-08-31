5060ef8c14daac69f06ff0998d5a01e62ac4dfd8
Gary Grubbs <gary.grubbs@gmail.com>
2022-08-31 14:17:37 -0500

    Removed drop shadow in footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { GitHubIcon } from '@/components/GitHubIcon'
 
 export function Footer() {
 	return (
-		<footer className="z-50 flex flex-wrap items-center justify-between bg-white px-4 py-2 shadow-md shadow-slate-900/5 transition duration-500 dark:shadow-none sm:px-6 lg:px-8 dark:bg-slate-900/95 dark:backdrop-blur dark:[@supports(backdrop-filter:blur(0))]:bg-slate-900/75">
+		<footer className="z-50 flex flex-wrap items-center justify-between bg-white px-4 py-2 sm:px-6 lg:px-8 dark:bg-slate-900/95 dark:backdrop-blur dark:[@supports(backdrop-filter:blur(0))]:bg-slate-900/75">
 		<div>
 			<p className="prose pl-2 text-sm dark:text-slate-400">&copy; 2022 HatTrick Labs, LLC. All rights reserved.</p>
 		</div>
