adbf435a72bd0a8b90d8ba584c9e2d9f308ecf1b
Gary Grubbs <gary.grubbs@gmail.com>
2022-09-19 10:36:23 -0500

    Moving GA id to GitHub action workflow

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -70,14 +70,14 @@ export default function App({ Component, pageProps }) {
         <meta property="og:image" content="/dbexpression-og-image.png" />
         <meta property="twitter:image" content="/dbexpression-twitter-image.png" />
       </Head>
-      <Script key="ga-script" strategy='afterInteractive' src="https://www.googletagmanager.com/gtag/js?id=G-BND29F2QPK" />
+      <Script key="ga-script" strategy='afterInteractive' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} />
       <Script id="ga-script-local" strategy='afterInteractive'>
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
         
-          gtag('config', 'G-BND29F2QPK');
+          gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
         `}
       </Script>
       <Layout title={title} tableOfContents={tableOfContents} >
