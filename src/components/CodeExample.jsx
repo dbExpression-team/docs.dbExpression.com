55bd312380346a3bd99a42ccace362e4eb37a123
Gary Grubbs <gary.grubbs@gmail.com>
2022-09-20 11:23:09 -0500

    Fixed react issue with code example control.

--- src/components/CodeExample.jsx
+++ src/components/CodeExample.jsx
@@ -1,32 +1,10 @@
 import { Fragment } from 'react'
 import { Tab } from '@headlessui/react'
 
-export function CodeExample({children}) {
+export function CodeExample({labels, children}) {
 
   function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
-  }
-
-  let labels = []
-  if (Array.isArray(children)) {
-    children.filter(p => p.type.name == 'Fence').map(t => labels.push(t.props.language == 'csharp' ? 'C#' : 'SQL'))
-  } else if (children.type.name == 'Fence') {
-    labels.push(children.props.language == 'csharp' ? 'C#' : 'SQL')
-  }
-
-  let content = []
-  if (Array.isArray(children)) {
-    for (let i = 0; i < children.length; i++) {
-      let child = children[i]
-      if (child.type.name == 'Fence' || i == 0) {
-        content.push([ child ])
-      }
-      else {
-        content[i - 1].push(child)
-      }
-    }
-  } else {
-    content.push([ children ])
   }
 
   return (
@@ -34,7 +12,7 @@ export function CodeExample({children}) {
       <div className='shadow-sm dark:shadow-none dark:border-slate-800/75 border rounded-xl mb-8'>
       <Tab.List className='px-4'>
         {labels.map((label, idx) =>
-          <Tab as={Fragment} key={idx}>{({ selected }) => (
+          <Tab as={Fragment} key={idx}>{({selected}) => (
             <button
             className={classNames('whitespace-nowrap py-2 px-8 border-b-2 font-medium text-sm focus:outline-none',
               selected
@@ -48,8 +26,8 @@ export function CodeExample({children}) {
         )}        
       </Tab.List>
       <Tab.Panels>
-        {content.map((c, idx) =>
-          <Tab.Panel key={idx} className='-mt-6 -mb-6'>{c}</Tab.Panel>
+        {children.map((content, idx) =>
+          <Tab.Panel key={idx} className='-mt-6 -mb-6'>{content}</Tab.Panel>
         )}  
       </Tab.Panels>
       </div>
