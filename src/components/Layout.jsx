6a419f1ef69bee52a454a1cf69fd709d1ed05ac6
Gary Grubbs <gary.grubbs@gmail.com>
2022-10-23 12:01:44 -0500

    decreased top padding for mobile

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -534,7 +534,7 @@ export function Layout({ children, title, tableOfContents }) {
       <Header navigation={navigation} />
 
       <div className="relative mx-auto flex max-w-8xl justify-center sm:px-2">
-        <div className="hidden lg:relative lg:block lg:flex-none">
+        <div className="hidden lg:relative lg:block lg:flex-none lg:py-12 ">
           <div className="absolute inset-y-0 right-0 w-[50vw] bg-slate-50 dark:hidden" />
           <div className="sticky top-[4.5rem] -ml-0.5 h-[calc(100vh-4.5rem)] overflow-y-auto pb-16 pt-4 pl-0.5 scrollbar">
             <div className="absolute top-16 bottom-0 right-0 hidden h-12 w-px bg-gradient-to-t from-slate-800 dark:block" />
@@ -545,7 +545,7 @@ export function Layout({ children, title, tableOfContents }) {
             />
           </div>
         </div>
-        <div className="min-w-0 max-w-2xl flex-auto px-4 py-16 lg:max-w-none lg:pr-0 lg:pl-12 xl:px-16">
+        <div className="min-w-0 max-w-2xl flex-auto px-4 py-4 lg:py-8 xl:py-16 lg:max-w-none lg:pr-0 lg:pl-12 xl:px-16">
           <article>
             {(title || currentPage?.breadcrumb) && (
               <header className="mb-9 space-y-1">
