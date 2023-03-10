c2f41194985dae992baf6461854dbe7c9b7592fd
Gary Grubbs <gary.grubbs@gmail.com>
2023-03-10 12:24:27 -0600

    Updated copyright year in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export function Footer() {
 	return (
 		<footer className="z-50 flex flex-wrap items-center justify-between bg-white px-4 py-2 sm:px-6 lg:px-8 dark:bg-slate-900/95 dark:backdrop-blur dark:[@supports(backdrop-filter:blur(0))]:bg-slate-900/75">
 		<div>
-			<p className="prose pl-2 text-sm dark:text-slate-400">&copy; 2022 HatTrick Labs, LLC. All rights reserved.</p>
+			<p className="prose pl-2 text-sm dark:text-slate-400">&copy; {new Date().getFullYear()} HatTrick Labs, LLC. All rights reserved.</p>
 		</div>
 		<div>        
 			<Link href="https://github.com/HatTrickLabs/dbExpression" className="group" aria-label="GitHub">
