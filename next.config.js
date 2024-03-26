f6a9c07702f9f71360987d9bf12747cd1d03de0a
gwgrubbs <gary.grubbs@gmail.com>
2024-03-26 13:11:26 -0500

    Changed NextJS config for deploying to Vercel.

--- next.config.js
+++ next.config.js
@@ -2,11 +2,22 @@ const withMarkdoc = require('@markdoc/next.js')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/docs.dbExpression.com",
   reactStrictMode: true,
-  images: { unoptimized: true },
+  pageExtensions: ['js', 'jsx', 'md'],
+  async redirects() {
+    return [
+      {
+        source: '/MsSql/Versions',
+        destination: '/mssql/versions',
+        permanent: true
+      },
+      {
+        source: '/rtd/reference/mssql/:path*',
+        destination: '/reference/mssql/:path*',
+        permanent: false
+      }
+    ]
+  }
 }
 
-//module.exports = withMarkdoc()(nextConfig)
 module.exports = withMarkdoc({mode: 'static'})(nextConfig)
